9806333b2f4c4d3709ec3be7ec23765f2a089cff
adityasuman2025 <adityasuman2025@gmail.com>
2024-03-21 07:45:46 +0530

    caching util upadted

--- src/lib/cachingUtils.ts
+++ src/lib/cachingUtils.ts
@@ -1,14 +1,18 @@
-export function getCacheRegular(key: string, defaultVal?: string) {
+export function getCacheRegular(key: string, defaultVal: any = null) {
     try {
         const str = localStorage.getItem(key);
-        return JSON.parse(str || defaultVal || "{}");
-    } catch (e) { }
-    return JSON.parse(defaultVal || "{}");
+        if (str) return JSON.parse(str);
+        else return defaultVal;
+    } catch (e) {
+        return defaultVal;
+    }
 }
 
 export function setCacheRegular(key: string, val: any) {
     try {
-        const str = JSON.stringify(val || {});
+        if (!val || !key) return;
+
+        const str = JSON.stringify(val);
         localStorage.setItem(key, str);
     } catch (e) { }
 }
--- src/lib/encCachingUtils.ts
+++ src/lib/encCachingUtils.ts
@@ -1,19 +1,23 @@
 import { decryptText, encryptText } from "./encryptionUtils";
 
-export function getCachedFromLStorage(key: string, encryptionKey: string, defaultVal?: string) {
+export function getCachedFromLStorage(key: string, encryptionKey: string, defaultVal: any = null) {
     try {
         const encrStr = localStorage.getItem(key);
         if (encrStr) {
             const str = decryptText(encrStr, encryptionKey);
-            return JSON.parse(str || defaultVal || "{}");
-        }
-    } catch (e) { }
-    return JSON.parse(defaultVal || "{}");
+            if (str) return JSON.parse(str);
+            else return defaultVal;
+        } else return defaultVal;
+    } catch (e) {
+        return defaultVal;
+    }
 }
 
 export function cacheInLStorage(key: string, val: any, encryptionKey: string) {
     try {
-        const str = JSON.stringify(val || {});
+        if (!val || !key || !encryptionKey) return;
+
+        const str = JSON.stringify(val);
         const encrStr = encryptText(str, encryptionKey);
         localStorage.setItem(key, encrStr);
     } catch (e) { }
